ffdb89668d11388bfe42a515f4542bbef34481c1
Jefferson Farias <jefferson.farias7@gmail.com>
2019-07-14 20:22:40 -0300

    correção

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['https://pontos-turisticos-jeff.herokuapp.com']
+ALLOWED_HOSTS = ['pontos-turisticos-jeff.herokuapp.com', 'localhost:8000']
 
 
 # Application definition
